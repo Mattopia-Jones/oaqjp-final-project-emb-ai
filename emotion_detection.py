e66eb4bb718a3b8b43f37386a822c86c453b8bd6
Mattopia-Jones <mattopiajones@gmail.com>
2026-04-10 13:42:13 -0600

    Enhance response with emotion scores and clean up code
    Added emotion scores to the response object and removed commented-out code for clarity.

--- emotion_detection.py
+++ emotion_detection.py
@@ -14,6 +14,15 @@ def emotion_detector(text_to_analyse):
         }
     }
 
+    {
+    'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    'dominant_emotion': '<name of the dominant emotion>'
+    }
+
     # Set the headers required for the API request
     header = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
@@ -23,23 +32,3 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json=myobj, headers=header)
 
     return response.text 
-
-    # Handle response
-#    if response.status_code == 200:
-        # Parse the response from the API
-#       formatted_response = json.loads(response.text)
-
-        # Extract sentiment label and score
-#        label = formatted_response['documentSentiment']['label']
-#        score = formatted_response['documentSentiment']['score']
-
-#    elif response.status_code == 500:
-#       label = None
-#        score = None
-
-#    else:
-#        label = None
-#        score = None
-
-    # Return the result
-#    return {'label': label, 'score': score}
