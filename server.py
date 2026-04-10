5b9a34087726c44c7e00ef696236446c79cfe362
Mattopia-Jones <mattopiajones@gmail.com>
2026-04-10 16:20:24 -0600

    Fix variable name and update server port

--- server.py
+++ server.py
@@ -32,10 +32,10 @@ def emote_detector():
     fear_score = response['fear']
     joy_score = response['joy']
     sadness_score = response['sadness']
-    dominant_score_score = response['dominant_emotion']
+    dominant_emotion = response['dominant_emotion']
 
     # Return a formatted string with all emotion scores
-    return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score}, 'sadness': {sadness_score}. The dominant emotion is {dominant_score}."
+    return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score}, 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
@@ -47,4 +47,4 @@ def render_index_page():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5005)
+    app.run(host="0.0.0.0", port=5000)
